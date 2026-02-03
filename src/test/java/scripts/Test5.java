97207666e41caa2698fee63dcdd33dffa044578e
Nagesh Sontakke <Nagesh Sontakke@DESKTOP-MUA6PUI>
2026-02-03 22:02:22 +0530

    hii

---
+++ src/test/java/scripts/Test5.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	
+@Test
+	
+	public void tc5() {
+		Reporter.log("Hello Automation",true);
+	}
+
+}
